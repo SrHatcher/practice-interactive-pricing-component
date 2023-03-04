e9eb600df70c5b64debe681f0233e4593ab22921
SrHatcher <erickomaldonado11@gmail.com>
2023-03-04 17:18:36 -0600

    desktop display completed, now working on dynamic spans with JS

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mySlider = document.querySelector('.upper_level__range_input')
+const billSpan = document.querySelector('.upper_level__bill_amount')
 
 function slider(){
     let valPercent = (mySlider.value / mySlider.max) * 100
-
+    
     mySlider.style.background = `linear-gradient(to right, var(--soft-cyan-full-slider-bar) ${valPercent}%, var(--empty-slider-bar) ${valPercent}%)`
 }
