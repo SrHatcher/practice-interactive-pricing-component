d3b4de984be58be2a9b416c5f7aee001caa02792
SrHatcher <erickomaldonado11@gmail.com>
2023-03-04 18:44:42 -0600

    dynamic spans now working, project completed

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {pageviews: "10K", bill: 8},
+    {pageviews: "50K", bill: 12},
+    {pageviews: "100K", bill: 16},
+    {pageviews: "500K", bill: 24},
+    {pageviews: "1M", bill: 32},
+]
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const mySlider = document.querySelector('.upper_level__range_input')
 const billSpan = document.querySelector('.upper_level__bill_amount')
+const viewsSpan = document.querySelector('.upper_level__pageviews_amount')
+const checkbox = document.querySelector('.options__switch_input')
+
 
 function slider(){
     let valPercent = (mySlider.value / mySlider.max) * 100
-    
+
+    changeSpans()
+
     mySlider.style.background = `linear-gradient(to right, var(--soft-cyan-full-slider-bar) ${valPercent}%, var(--empty-slider-bar) ${valPercent}%)`
+}
+
+function changeSpans(){
+    const slideValue = mySlider.value
+    
+    if(checkbox.checked){
+        let discount = data[slideValue].bill * 0.25
+        let totalBill = data[slideValue].bill - discount
+
+        billSpan.innerText = `$${totalBill}.00`
+        viewsSpan.innerText = `${data[slideValue].pageviews} PAGEVIEWS`
+        return
+    }
+
+    billSpan.innerText = `$${data[slideValue].bill}.00`
+    viewsSpan.innerText = `${data[slideValue].pageviews} PAGEVIEWS`
 }
